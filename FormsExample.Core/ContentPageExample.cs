b4fe61f78e93991375036cb72077b41043162d66
Rui Oliveira Pinheiro <rui.pinheiro@together.pt>
2016-05-22 20:17:01 +0100

    Page 30. So far, it works and runs in the Android mobile.

---
+++ FormsExample.Core/ContentPageExample.cs
@@ -0,0 +1,46 @@
+﻿using Xamarin.Forms;
+
+namespace FormsExample.Core
+{
+    public class ContentPageExample : ContentPage
+    {
+        public ContentPageExample()
+        {
+            Label labelLarge = new Label
+            {
+                Text = "Label",
+                // FontSize = 40,
+                FontSize = Device.GetNamedSize( NamedSize.Large, typeof( Button ) ),
+                HorizontalOptions = LayoutOptions.Center,
+                TextColor = Color.White,
+                FontFamily = "Courier",
+                FontAttributes = FontAttributes.Bold
+            };
+
+            Label labelSmall = new Label
+            {
+                Text = "This control is great for\n"
+                     + "displaying one or more\n"
+                     + "lines of text.",
+                // FontSize = 20,
+                FontSize = Device.GetNamedSize( NamedSize.Medium, typeof( Button ) ),
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                TextColor = Color.White,
+                FontAttributes = FontAttributes.Italic
+            };
+
+            StackLayout stackLayout = new StackLayout
+            {
+                Children =
+                {
+                    labelLarge,
+                    labelSmall
+                },
+                HeightRequest = 1500,
+            };
+
+            this.Content = stackLayout;
+            this.BackgroundColor = Color.Black;
+        }
+    }
+}
