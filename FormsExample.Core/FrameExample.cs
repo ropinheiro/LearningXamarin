f9848db32011d2609f68920dd6325e696033eedb
Rui Oliveira Pinheiro <rui.pinheiro@together.pt>
2016-06-22 08:48:08 +0100

    Pages 83 to 84. Learned how to use Frame.

---
+++ FormsExample.Core/FrameExample.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+
+namespace FormsExample.Core
+{
+    // Pages 83 to 84
+    public class FrameExample : ContentPage
+    {
+        public FrameExample()
+        {
+            // Page 83
+            this.Padding = 20;
+            this.Content = new Frame
+            {
+                Content = new Label { Text = "Framed", FontSize = 40 },
+                OutlineColor = Color.Red
+            };
+        }
+    }
+}
