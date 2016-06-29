2a0ff17df0e4d6a81bac292dc44f2260431938eb
Rui Oliveira Pinheiro <rui.pinheiro@together.pt>
2016-06-29 14:29:20 +0100

    Pages 154 to 160 - Learned to use ListView and binding it to DataModel.

---
+++ FormsExample.Core/ListViewDataModel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace FormsExample.Core
+{
+    // Pages 157 to 160
+    public class ListViewDataModel : ContentPage
+    {
+        public ListViewDataModel()
+        {
+            // Page 157
+            ListView listView = new ListView();
+            listView.ItemsSource = new ListItem[]
+                {
+                    new ListItem { Title="First", Description="1st item" },
+                    new ListItem { Title="Second", Description="2nd item" },
+                    new ListItem { Title="Third", Description="3rd item" }
+                };
+
+            // Page 158
+            listView.ItemTemplate = new DataTemplate( typeof( TextCell ) );
+            listView.ItemTemplate.SetBinding( TextCell.TextProperty, "Title" );
+            listView.ItemTemplate.SetBinding( TextCell.DetailProperty, "Description" );
+            listView.ItemTemplate.SetValue( TextCell.TextProperty, Color.Red );
+            listView.ItemTemplate.SetValue( TextCell.DetailColorProperty, Color.Blue );
+
+            // Page 159
+            listView.ItemTapped += async ( sender, e ) =>
+            {
+                ListItem item = (ListItem)e.Item;
+                await DisplayAlert( "Tapped", item.Title.ToString() + " was tapped.", "OK" );
+                ( (ListView)sender ).SelectedItem = null;
+            };
+
+            listView.ItemSelected += async ( sender, e ) =>
+            {
+                if ( e.SelectedItem == null ) return;
+                ListItem item = (ListItem)e.SelectedItem;
+                await DisplayAlert( "Selected", item.Title.ToString() + " was selected.", "OK" );
+                ( (ListView)sender ).SelectedItem = null;
+            };
+
+            this.Padding = new Thickness( 0, Device.OnPlatform( 20, 0, 0 ), 0, 0 );
+            this.Content = listView;
+        }
+    }
+
+    // Page 157
+    public class ListItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ FormsExample.Core/ListViewStrings.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace FormsExample.Core
+{
+    // Pages 154 to 157
+    public class ListViewStrings : ContentPage
+    {
+        public ListViewStrings()
+        {
+            // Page 154
+            ListView listView = new ListView();
+            List<string> items = new List<string>() { "First", "Second", "Third" };
+            listView.ItemsSource = items;
+
+            // Page 155
+            listView.ItemTapped += async ( sender, e ) =>
+            {
+                await DisplayAlert( "Tapped", e.Item.ToString() + " was tapped.", "OK" );
+                ( (ListView)sender ).SelectedItem = null;           // Page 156
+            };
+
+            listView.ItemSelected += async ( sender, e ) =>
+            {
+                if ( e.SelectedItem == null ) return;          // Page 156
+                await DisplayAlert( "Selected", e.SelectedItem.ToString() + " was selected.", "OK" );
+                ( (ListView)sender ).SelectedItem = null;           // Page 156
+            };
+
+            // Page 155
+            this.Padding = new Thickness( 0, Device.OnPlatform( 20, 0, 0 ), 0, 0 );
+
+            this.Content = listView;
+        }
+    }
+}
